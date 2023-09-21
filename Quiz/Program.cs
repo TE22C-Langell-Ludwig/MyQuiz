40345a347f264a1fd9475e083e017e10cfcb6503
TE22A-Assal-Yousuf <yousuf.assal@elev.ga.ntig.se>
2023-09-21 15:53:39 +0200

    cool

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -6,13 +6,31 @@ int right = 0;
 //Här ligger all kod
 
 Console.WriteLine("Question 1: ");
+Console.WriteLine("who won the 2022 world cup, Argentina, France or Japan");
 string answer = Console.ReadLine(). ToLower();
-if (answer == "yes"){
-    Console.WriteLine("wow you are stupid that is the wrong answer, you lose!");
+if (answer == "argentina"){
+    Console.WriteLine("That's correct, Wow you know so much so i guess you enjoy watching football");
+    points += 12;
+    right++;
+}
+else if(answer == "france" || answer == "fapan"){
+    Console.WriteLine("close but that was not the right answere");
 
 }
-else if(answer == "no"){
-    Console.WriteLine("Nice job i can tell that you're a real swede");
+else{
+    Console.WriteLine("That was not one of the options given to you, so you forfeit the question");
+}
+
+Console.WriteLine($"Question 2: ");
+Console.WriteLine("Could you teach me jappanese");
+string answer2 = Console.ReadLine(). ToLower();
+if (answer2 == "yes"){
+    Console.WriteLine("Oh cool, we'll take that later");
+    points += 12;
+    right++;
+}
+else if(answer2 == "no"){
+    Console.WriteLine("damn i guess im out of luck");
     points += 12;
     right++;
 }
@@ -20,13 +38,50 @@ else{
     Console.WriteLine("That was not one of the options given to you, so you forfeit the question");
 }
 
-Console.WriteLine($"Question 2: ");
-
-
 Console.WriteLine($"Question 3: ");
+Console.WriteLine("What is the Swedish capital");
+string answer3 = Console.ReadLine(). ToLower();
+if (answer3 == "stockholm"){
+    Console.WriteLine("Nice thats correct");
+    points += 12;
+    right++;
+}
+else{
+    Console.WriteLine("That was the wrong answere");
+}
+
+Console.WriteLine($"Question 4: ");
+Console.WriteLine("What is Koharu's favourite food?");
+Console.WriteLine("Meatballs");
+Console.WriteLine("Liquorice");
+string answer4 = Console.ReadLine(). ToLower();
+if (answer4 == "meatballs"){
+    Console.WriteLine("Nice thats correct");
+    points += 12;
+    right++;
+}
+else{
+    Console.WriteLine("That was the wrong answere");
+}
+
+Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if (right == 1){
-    Console.WriteLine($"you completed this quiz with only {right} questions right and {points} point better luck next time");
+    Console.WriteLine($"you completed this quiz with only {right} questions right and {points} points better luck next time");
 }
 else if(right == 2){
     Console.WriteLine($"Nice you ended the quiz with {right} questions right and {points} points you almost got the highest score");
@@ -34,4 +89,5 @@ else if(right == 2){
 else if(right == 3){
     Console.WriteLine($"WOW you ended the quiz with {right} questions right and {points} points you got the highest score 100IQ moves!");
 }
+
 Console.ReadLine();
